54e16cdae5ec5a92dcf1e98ebd17009225729188
darikmohammed <r.m.darik@gmail.com>
2024-01-11 08:59:49 +0300

    Init with ruby file

---
+++ db/migrate/20240111055931_create_articles.rb
@@ -0,0 +1,11 @@
+class CreateArticles < ActiveRecord::Migration[7.0]
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.string :publisher
+      t.integer :published_year
+
+      t.timestamps
+    end
+  end
+end
