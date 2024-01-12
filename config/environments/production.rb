47aec90752d79e4170a75d84b914af83eb4ca735
darikmohammed <r.m.darik@gmail.com>
2024-01-12 06:37:55 +0300

    update config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,6 +15,8 @@ Rails.application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
+  config.host_authorization = { exclude: ->(request) { request.path =~ /healthcheck/ } }
+
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
